7e3ec7f4fe495bd0ca4a2f1a39c2fa2590fe480c
martin.daguerre <martin.daguerre@wework.com>
2018-07-22 00:13:58 -0300

    add chart showing temperature pulled from firebase db

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { FirebaseConfig } from './config/firebase';
 import * as _ from 'lodash';
 import * as moment from 'moment';
 import './App.css';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
@@ -17,7 +18,8 @@ interface TempModel {
 interface TemperatureRead {
   temp: number;
   humidity: number;
-  date: string;
+  dateStr: string;
+  date: moment.Moment;
 }
 interface Props {
 }
@@ -31,6 +33,8 @@ interface State {
 
 class App extends Component<Props, State> {
 
+  private interval: NodeJS.Timer;
+
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -39,32 +43,11 @@ class App extends Component<Props, State> {
     };
   }
 
-  componentWillMount() {
-    let data: TemperatureRead[] = [];
+  componentDidMount() {
     firebase.initializeApp(FirebaseConfig);
-    // firebase.database().ref('home/temp').once('value').then((snapshot: firebase.database.DataSnapshot) => {
-    //   _.forEach(snapshot.val(), (read: TempModel) => {
-        
-    //     if (read.humidity === 0) {
-    //       return;
-    //     }
-
-    //     const date = moment.unix(read.date);
-
-    //     data.push({
-    //       temp: read.temp,
-    //       humidity: read.humidity,
-    //       date: date.format('DD-MM-YY H:mm:s')
-    //     });
-    //   });
-
-    //   this.setState({
-    //     data: data
-    //   });
-    // });
-
+    
     firebase.database().ref('home/temp').on('value', (snapshot: firebase.database.DataSnapshot) => {
-      console.log('add');
+      const data: TemperatureRead[] = [];
       let previousTemp: number|null = null;
       
       _.forEach(snapshot.val(), (read: TempModel) => {
@@ -83,9 +66,15 @@ class App extends Component<Props, State> {
         data.push({
           temp: read.temp,
           humidity: read.humidity,
-          date: date.format('DD-MM-YY H:mm:s')
+          dateStr: date.format('DD-MM-YY H:mm:s'),
+          date: date
         });
+
       });
+
+      const totalTemp = _.sumBy(data, 'temp');
+      const avgTemp = totalTemp / data.length - 1;
+      console.log('Avg Temp: ' + avgTemp);
 
       this.setState({
         data: data
@@ -94,9 +83,16 @@ class App extends Component<Props, State> {
     }, (errorObject: any) => {
       console.log('The read failed: ' + errorObject.code);
     });
+
+    this.interval = setInterval(this.updateLastRead, 3000);
   }
 
-  updateLastRead(): void {
+  componentWillUnmount() {
+    // remove the interval listener
+    clearInterval(this.interval);
+  }
+
+  updateLastRead = (): void => {
     const length = this.state.data.length;
 
     this.setState({
@@ -107,28 +103,57 @@ class App extends Component<Props, State> {
 
   public render() {
     return (
-      <div className="App">
-        <LineChart 
-            width={600} 
-            height={300} 
-            data={this.state.data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}
-        >
-            <XAxis dataKey="date"/>
-            <YAxis/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Legend />
-            <Line type="monotone" dot={false} dataKey="temp" stroke="#8884d8" />
-            <Line type="monotone" dot={false} dataKey="humidity" stroke="#82ca9d" />
-        </LineChart>
-        { this.state.lastRead != null && ( 
-        <div>
-          Temp: {this.state.lastRead.temp}
-          H: {this.state.lastRead.humidity}
-        </div>
-        )}
-      </div>
+      <Grid className="App">
+        <Row className="data-container">
+          <Col xs={6} md={7} className="">
+
+          <Panel bsStyle="info">
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">
+                Upstairs
+                { this.state.lastRead != null && (
+                  <div className="info-container"> 
+                   <div className="info-row">                
+                      <small>{this.state.lastRead.date.fromNow()}</small>
+                    </div>
+                   
+                    <div className="info-row">                
+                      <i className="fas fa-tint" />
+                      <span>
+                        {`${this.state.lastRead.humidity}%`}
+                      </span>
+                    </div>
+                   
+                    <div className="info-row">
+                      <i className="fas fa-thermometer-half" />
+                      <span>
+                        {`${this.state.lastRead.temp} °C`}
+                      </span>
+                    </div>
+                  </div>
+                  )}
+              </Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>
+              <LineChart 
+                  width={600} 
+                  height={200} 
+                  data={this.state.data.slice()}
+                  margin={{top: 5, right: 30, left: 20, bottom: 5}}
+              >
+                  <XAxis dataKey="dateStr"/>
+                  <YAxis/>
+                  <CartesianGrid strokeDasharray="3 3"/>
+                  <Tooltip/>
+                  <Legend />
+                  <Line type="monotone" dot={false} dataKey="temp" stroke="#8884d8" />
+                  <Line type="monotone" dot={false} dataKey="humidity" stroke="#82ca9d" />
+              </LineChart>
+            </Panel.Body>
+          </Panel>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
